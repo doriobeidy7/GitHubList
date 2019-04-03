ecb7e35fe2871d95a2b0f938f8e2b64e820f8d69
Dori Obeidy <doriobeidy@gmail.com>
2019-04-03 12:03:10 +0300

    Initial Commit

---
+++ GitHubList/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GitHubList
+//
+//  Created by Dori on 4/3/19.
+//  Copyright © 2019 Dori. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
